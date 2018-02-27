f193579e7973e3630030b5c6fc2dc60a0406f791
John Marshall <marshall@hep.phy.cam.ac.uk>
2018-02-27 13:35:21 +0000

    Disable old v04_16_00 tests.

--- go.sh
+++ go.sh
@@ -7,4 +7,4 @@ source $CRON_HOME/run/update.sh
 source $CRON_HOME/run/build.sh
 source $CRON_HOME/run/run.sh v05_08_00
 #source $CRON_HOME/run/run.sh v05_04_00
-source $CRON_HOME/run/run.sh v04_16_00
+#source $CRON_HOME/run/run.sh v04_16_00

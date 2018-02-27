f100f5b472a651f0f0cd03e6e49a91f14418b0b3
John Marshall <marshall@hep.phy.cam.ac.uk>
2018-02-27 13:33:48 +0000

    Move to root v6_06_08.

--- setup.sh
+++ setup.sh
@@ -3,11 +3,11 @@
 export CRON_HOME=/usera/marshall/Test/cron
 
 source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
-setup gcc v4_9_3 -f Linux64bit+2.6-2.12
-setup git v2_4_6 -f Linux64bit+2.6-2.12
+setup gcc v4_9_3
+setup git v2_4_6
+#setup python v2_7_13d
 setup eigen v3_3_3
-setup root v5_34_32 -f Linux64bit+2.6-2.12 -q e9:nu:prof
-#setup root v6_06_08 -f Linux64bit+2.6-2.12 -q e10:nu:prof
+setup root v6_06_08 -q e10:nu:prof
 
 export LD_LIBRARY_PATH=${CRON_HOME}/lib/:$LD_LIBRARY_PATH
 
